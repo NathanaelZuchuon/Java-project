934bada6c7d7f5e63b85f2c5655635e5af4b2634
Nathanael Zuchuon <nzuchuon@gmail.com>
2021-08-11 21:51:47 +0100

    My first Java project

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner var = new Scanner(System.in);
+        System.out.println("Welcome " + var.next() + '!');
+    }
+}
